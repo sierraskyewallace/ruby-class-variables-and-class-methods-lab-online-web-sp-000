c7791fc4a24e26ba63c31ef4c7f6e83627b28f28
Sierra Wallace <sskwallace@gmail.com>
2020-09-06 01:47:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,15 @@
 class Song 
   attr_accessor :name, :artist, :genre 
   @@count = 0 
+  @@artist = [] 
+  @@genre = []
   
   def initialize(name, artist, genre)
     @name = name 
     @artist = artist 
     @genre = genre 
     @@count += 1 
+    @@artist << artist
+    @@genre << genre
   end
 end
