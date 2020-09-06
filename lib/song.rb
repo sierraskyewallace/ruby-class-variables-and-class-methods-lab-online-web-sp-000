d39758f4243904397a73d307871ccbe577892b0d
Sierra Wallace <sskwallace@gmail.com>
2020-09-06 01:52:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,6 +30,14 @@ class Song
     @@genres.each {|genre| genre_count[genre] += 1}
     genre_count
   end
+  
+  def self.artist_count 
+    artist_count = Hash.new(0)
+    @@artists.each {|artist| artist_count[artist] += 1}
+    artist_count
+  end
+end
+
 end
 
 
